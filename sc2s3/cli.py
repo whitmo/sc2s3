eae17b8559af0fea9028dfe985ea304b7aa07fd8
whitmo <d.w.morriss@gmail.com>
2013-03-08 10:33:00 -0600

    basic working cli

--- sc2s3/cli.py
+++ sc2s3/cli.py
@@ -1,35 +1,74 @@
-import grequests
-import json
-import logging
-import requests
-import urlparse
+from .sc import list_urls
 from cStringIO import StringIO as strio
+from cliff.app import App
+from cliff.commandmanager import CommandManager
+from cliff.lister import Lister
+from itertools import count
+import logging
+import pkg_resources
+import requests
+import sys
+import urllib
+
+
+class CLIApp(App):
+    """
+    command line interface
+    """
+    specifier = 'sc2s3.cli'
+    version = pkg_resources.get_distribution('sc2s3').version
+    log = logging.getLogger(__name__)
+
+    def __init__(self):
+        super(CLIApp, self)\
+            .__init__(description=self.specifier,
+                      version=self.version,
+                      command_manager=CommandManager(self.specifier))
+
+
+class SCUrls(Lister):
+    list_urls = staticmethod(list_urls)
+    def get_parser(self, prog_name):o
+        parser = super(SCUrls, self).get_parser(prog_name)
+        parser.add_argument('clientid', 
+                            help="Sound cloud client id")
+        parser.add_argument('username', 
+                            help="Sound cloud username")
+        return parser
+
+    def take_action(self, pargs):
+        """
+        enqueue a job
+        """
+        return self.list_urls(pargs.clientid, pargs.username)
+
+        
+def main(argv=sys.argv[1:], app=CLIApp):
+    return app().run(argv)
+
 
 logger = logging.getLogger(__name__)
 
-
-def track_data(client_id, username):
-    url = 'http://api.soundcloud.com/users/{username}/tracks.json?client_id={id}'.format(**locals())
-    res = requests.get(url)
-    if res.status_code == 200:
-        tracks = json.load(res.content)
-        return tracks, True
-    return None, res
-
-
-def trax2urls(trax):
-    for track in trax:
-        if 'downloadable' in track:
-           yield track['download_url']
+#ced262b670362c8fc4d307f0d20a3d48
 
 
 
-def download_upload(url, bucket, redundancy=True):
+
+
+
+
+def make_key(track_md):
+    title = track_md['title']
+    id_ = track_md['id']
+    return "{}-{}".format(id_, urllib.quote(title))
+
+
+def download_upload(url, key, bucket, extra={}, redundancy=True):
     res = requests.get(url, stream=True)
-    mp = bucket.initiate_multipart_upload(fpath, reduced_redundancy=not redundancy)
+    mp = bucket.initiate_multipart_upload(key, metadata=extra, reduced_redundancy=not redundancy)
+    idx = count()
     for block in res.iter_content:
-
-        pass
+        mp.upload_part_from_file(strio(block), next(idx))
 
 
 def stream_to_s3():
@@ -40,3 +79,5 @@ def stream_to_s3():
 #https://gist.github.com/mattbillenstein/1471499
 #http://blog.odonnell.nu/posts/streaming-uploads-s3-python-and-poster/
 #concurrent downloader
+
+
--- setup.py
+++ setup.py
@@ -1,26 +1,35 @@
-from setuptools import setup, find_packages
-import sys, os
+from setuptools import setup
+from setuptools import find_packages
+import sys 
+import os
 
 version = '0.0'
+
+requires = ['cliff',
+            'cliff-tablib',
+            'requests',
+            'boto']
 
 setup(name='sc2s3',
       version=version,
       description="Move files from soundcloud to S3",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      long_description=""" """,
+      classifiers=[], 
       keywords='',
       author='whit',
-      author_email='whit@nocoast.us',
+      author_email='whit at nocoast.us',
       url='http://whitmorriss.org/software/sc2s3',
       license='BSD',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
-      install_requires=[
-          # -*- Extra requirements: -*-
-      ],
+      install_requires=requires,
       entry_points="""
-      # -*- Entry points: -*-
+      [console_scripts]
+      sc2s3=sc2s3.cli:main
+
+      [sc2s3.cli]
+      urls=c2s3.cli:SCUrls
       """,
       )
+
